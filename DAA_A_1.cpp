ae9bdbd683d97b212231db4ce731ba6be1ce9a41
GirishT25 <151001374+GirishT25@users.noreply.github.com>
2024-09-25 10:53:54 +0530

    Add files via upload

--- DAA_A_1.cpp
+++ DAA_A_1.cpp
@@ -1,48 +1,26 @@
-
-/* 
-Implement a problem of move all zeroes to end of
-array.
-Statement: Given an array of random numbers, Push all the zero’s
-of a given array to the end of the array. For example, if the given
-arrays is {1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0}, it should be changed to {1, 9,
-8, 4, 2, 7, 6, 0, 0, 0, 0}. The order of all other elements should be
-same.
-Input : arr[] = {1, 2, 0, 4, 3, 0, 5, 0};
-Output : arr[] = {1, 2, 4, 3, 5, 0, 0, 0}; */
-
-
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main() {
-    int n;
-
-    cout << "Enter the number of elements: ";
-    cin >> n;
-
-    int arr[n];
-    cout << "Enter the array elements: ";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    int index = 0;  
-
-    for (int i = 0; i < n; i++) {
-        if (arr[i] != 0) {
-            arr[index++] = arr[i];
+int main(){
+    int a[]={1,1,1,0,0},count=0;
+    int low=0;
+    int size=end(a)-begin(a);
+    int high=size-1;
+    int mid;
+    while(low<=high){
+        mid=(low+high)/2;
+        if(a[mid]==1){
+            low=mid+1;
+            
         }
+        else{
+            high=mid-1;
+           
+        }
+      
     }
+    for(int i=mid+1;i<=size;i++)
+        count++;
 
-    
-    while (index < n) {
-        arr[index++] = 0;
-    }
-
-    cout << "Array after moving all zeros to the end: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-
+    cout<<"no of zero's are :"<<count;
     return 0;
 }
