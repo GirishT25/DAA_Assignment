ed3b554842df412579e6005736991e3e501c60d5
GirishT25 <151001374+GirishT25@users.noreply.github.com>
2024-12-04 12:32:32 +0530

    Update DAA_A_1.cpp

--- DAA_A_1.cpp
+++ DAA_A_1.cpp
@@ -1,26 +1,32 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int a[]={1,1,1,0,0},count=0;
-    int low=0;
-    int size=end(a)-begin(a);
-    int high=size-1;
-    int mid;
-    while(low<=high){
-        mid=(low+high)/2;
-        if(a[mid]==1){
-            low=mid+1;
-            
-        }
-        else{
-            high=mid-1;
-           
-        }
-      
-    }
-    for(int i=mid+1;i<=size;i++)
-        count++;
 
-    cout<<"no of zero's are :"<<count;
+int main() {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int a[size];
+    cout << "Enter " << size << " elements (only 0s and 1s): ";
+    for(int i = 0; i < size; i++) {
+        cin >> a[i];
+    }
+
+    int low = 0;
+    int high = size - 1;
+    int mid;
+
+    while(low <= high) {
+        mid = (low + high) / 2;
+        if(a[mid] == 1) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    int count = size - low;
+    cout << "Number of zeros are: " << count;
+
     return 0;
 }
